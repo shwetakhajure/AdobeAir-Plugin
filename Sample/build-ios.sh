d53c619530041bfc7a2196a34f8be4ab04f97287
shweta khajure <shwetakha@cybage.com>
2017-09-15 17:01:58 +0530

    updated certificate keys and amxmlc and adt paths

--- Sample/build-ios.sh
+++ Sample/build-ios.sh
@@ -4,13 +4,13 @@ set -e
 
 mkdir -p build
 
-amxmlc -compiler.library-path=../AirExtension/extensions/com.vungle.extensions.Vungle.ane \
+/Users/vungle.sdk/Desktop/ForAdobeAir/ApacheflexwithAirSDK/bin/amxmlc -compiler.library-path=../AirExtension/extensions/com.vungle.extensions.Vungle.ane \
 	-output build/sample.swf -swf-version=23 -default-size=320,480 \
 	-default-background-color=#ffffff -debug src/VungleExample.as
 
-adt -package -target ipa-test-interpreter -keystore keys/sample-ios.p12 \
-	-storetype pkcs12 -storepass 123456 \
-	-provisioning-profile keys/sample-ios.mobileprovision \
+/Users/vungle.sdk/Desktop/ForAdobeAir/ApacheflexwithAirSDK/bin/adt -package -target ipa-test-interpreter -keystore keys/iosdev.p12 \
+	-storetype pkcs12 -storepass vungleluv1 \
+	-provisioning-profile keys/macminibuilds.mobileprovision \
 	build/example.ipa app.xml -C build sample.swf \
 	-C src/assets VungleIcon.png VungleIcon@2x.png VungleLogo.png \
 	Default@2x.png Default-568h@2x.png \
