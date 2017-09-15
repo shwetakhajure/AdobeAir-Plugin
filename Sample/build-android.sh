cdd1be5a43cf3d853bfa7b148cf95ad9a8e4e148
shweta khajure <shwetakha@cybage.com>
2017-09-15 16:24:11 +0530

    chaged .p12 name and keystore password

--- Sample/build-android.sh
+++ Sample/build-android.sh
@@ -8,8 +8,8 @@ amxmlc -compiler.library-path=../AirExtension/extensions/com.vungle.extensions.V
 	-output build/sample.swf -swf-version=23 -default-size=320,480 \
 	-default-background-color=#ffffff -debug src/VungleExample.as
 
-adt -package -target apk-captive-runtime -keystore keys/sample-android.p12 \
-	-storetype pkcs12 -storepass 123456 build/example.apk app.xml \
+adt -package -target apk-captive-runtime -keystore keys/debug.p12 \
+	-storetype pkcs12 -storepass android build/example.apk app.xml \
 	-C build sample.swf \
 	-C src/assets VungleIcon.png VungleIcon@2x.png VungleLogo.png \
 	-extdir ../AirExtension/extensions
